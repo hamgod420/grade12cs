14035e69abaea086c87edd6fda653fe73ed90efc
hamgod420 <84872216+hamgod420@users.noreply.github.com>
2022-01-04 03:20:00 -0500

    Update FibonacciSequenceDP.java

--- Unit 3 DSA Dynamic Programming/Example Solutions/FibonacciSequenceDP.java
+++ Unit 3 DSA Dynamic Programming/Example Solutions/FibonacciSequenceDP.java
@@ -8,21 +8,24 @@ import java.util.*;
  */
 public class FibonacciSequenceDP {
 
+	public static int operations = 0;
+	
 	public static long findFib(int n) {
 		long dp[]=new long[n+2];//handles 0
 		dp[0]=0;
 		dp[1]=1;
 		for(int i = 2;i<=n;i++) {
 			dp[i]=dp[i-1]+dp[i-2];
+			operations += 1;
 		}
-		System.out.println(Arrays.toString(dp));
+		//System.out.println(Arrays.toString(dp));
 		return dp[n];
 	}
 	
 	public static void main(String[] args) {
 		
 		System.out.println(findFib(10));
-		
+		System.out.println(operations);
 	}
 
 }
