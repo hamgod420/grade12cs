1b94b2053ec78199f6e9f646e545283052ba2bea
hamgod420 <84872216+hamgod420@users.noreply.github.com>
2022-01-04 03:17:45 -0500

    Update FibonacciSequenceRecursion.java

--- Unit 3 DSA Dynamic Programming/Example Solutions/FibonacciSequenceRecursion.java
+++ Unit 3 DSA Dynamic Programming/Example Solutions/FibonacciSequenceRecursion.java
@@ -8,17 +8,22 @@ import java.util.*;
  */
 public class FibonacciSequenceRecursion {
 
+    public static int operations = 0;
+
 	public static int findFib(int n) {
 		if(n<=1) {
 			return n;
+		}else{
+		    int f = findFib(n-1)+findFib(n-2);
+		    operations += 1;
+		    return f;
 		}
-		return findFib(n-1)+findFib(n-2);
 	}
 	
 	public static void main(String[] args) {
 		
-		System.out.println(findFib(3));
-
+		System.out.println(findFib(10));
+        System.out.println(operations);
 	}
 
 }
