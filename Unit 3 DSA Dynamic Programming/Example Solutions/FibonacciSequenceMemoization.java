c33f326fbce2299e792eda3e9aa4c452e9364e10
hamgod420 <84872216+hamgod420@users.noreply.github.com>
2022-01-04 03:15:38 -0500

    Create FibonacciSequenceMemoization.java

---
+++ Unit 3 DSA Dynamic Programming/Example Solutions/FibonacciSequenceMemoization.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+import java.util.*;
+/**
+ * @author hamgod
+ *
+ */
+public class FibonacciSequenceMemoization {
+
+    public static int memo[] = new int[1000001];
+    public static int operations = 0;
+	public static int findFib(int n) {
+	    if(memo[n]!=0){
+	        return memo[n];
+	    }
+		if(n<=1) {
+			return n;
+		}else{
+		    int f = findFib(n-1)+findFib(n-2);
+		    operations += 1;
+		    memo[n] = f;
+		    return f;
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println(findFib(10));
+		System.out.println(operations);
+
+	}
+
+}
