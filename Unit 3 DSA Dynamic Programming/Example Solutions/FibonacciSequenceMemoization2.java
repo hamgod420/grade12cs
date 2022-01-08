faba0fb17ae22d27cbb7b799da644f47b473a493
hamgod420 <84872216+hamgod420@users.noreply.github.com>
2022-01-08 14:01:16 -0500

    Update FibonacciSequenceMemoization2.java

--- Unit 3 DSA Dynamic Programming/Example Solutions/FibonacciSequenceMemoization2.java
+++ Unit 3 DSA Dynamic Programming/Example Solutions/FibonacciSequenceMemoization2.java
@@ -7,21 +7,20 @@ import java.util.*;
  *
  */
 public class FibonacciSequenceMemoization2 {
-
-  public static HashMap<Integer,Integer> memo = new HashMap<>();
-  public static int operations = 1;
-    //<n,fib(n)>
-    public static int findFib(int n) {
-		
+	
+	public static HashMap<Integer,Integer> memo = new HashMap<>();
+	public static int operations = 1;
+	//<n,fib(n)>
+	public static int findFib(int n) {
 		if(memo.containsKey(n)){
-		    return memo.get(n);
+			return memo.get(n);
 		}
 		if(n<=1) {
-			f = n;
+			return n;
 		}else{
-            int f; = findFib(n-1)+findFib(n-2); 
-            operations += 1;
-            memo.put(n,f);
+			int f; = findFib(n-1)+findFib(n-2); 
+			operations += 1;
+			memo.put(n,f);
 		}
 		return f;
 	}
