2778da334d6b120cf7bcce7889b73d631950bb36
hamgod420 <84872216+hamgod420@users.noreply.github.com>
2022-01-08 21:24:28 -0500

    Update FactorialDP.java

--- Unit 3 DSA Dynamic Programming/Example Solutions/FactorialDP.java
+++ Unit 3 DSA Dynamic Programming/Example Solutions/FactorialDP.java
@@ -10,6 +10,9 @@ public class FactorialDP {
 
 	public static long findFactorial(int n) {
 		int dp[]=new int[n+1];
+		if(n == 0){
+		    return 1;    
+		}
 		dp[1]=1;
 		for(int i = 2;i<=n;i++) {
 			dp[i]=dp[i-1]*i;
