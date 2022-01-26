b60eb503f700dfe51d95923f740126e171deae37
hamgod420 <84872216+hamgod420@users.noreply.github.com>
2022-01-25 21:18:12 -0500

    Update Main.java

--- LeagueOfLegendsSystem/src/Main.java
+++ LeagueOfLegendsSystem/src/Main.java
@@ -8,6 +8,7 @@ import java.io.*;
  * 2021-11-25 
  * Creating Systems 
  * Main.java
+ * Player class / Testing class
  */
 public class Main {
 
@@ -16,14 +17,16 @@ public class Main {
      * Focus: Champion stats and damage output
      */
 	public static void main(String[] args) throws IOException {
+		//Player can simulate a champion with this program and test damage output
+		
 		//Kayn
-    	EntityStats KaynEntityStats = new EntityStats(921,92,0,57,37,0.74,5,0,345);
-    	ChampionStats KaynChampionStats = new ChampionStats(11,15,0,0,0,0,0,0,0);
-    	Ability ReapingSlash = new Ability("Reaping Slash",1,244,0,false);
-    	Ability BladesReach = new Ability("Blades Reach",1,104,0,false);
-    	Ability ShadowStep = new Ability("Shadow Step",0,0,0,false);
-    	Ability UmbralTrespass = new Ability("Umbral Trespass",1,169,0,false);
-    	Champion Kayn = new Champion("Kayn",KaynEntityStats,KaynChampionStats,ReapingSlash,BladesReach,ShadowStep,UmbralTrespass);;
+    	EntityStats kaynEntityStats = new EntityStats(921,92,0,57,37,0.74,5,0,345);
+    	ChampionStats kaynChampionStats = new ChampionStats(11,15,0,0,0,0,0,0,0);
+    	Ability reapingSlash = new Ability("Reaping Slash",1,244,0,false);
+    	Ability bladesReach = new Ability("Blades Reach",1,104,0,false);
+    	Ability shadowStep = new Ability("Shadow Step",0,0,0,false);
+    	Ability umbralTrespass = new Ability("Umbral Trespass",1,169,0,false);
+    	Champion kayn = new Champion("kayn",kaynEntityStats,kaynChampionStats,reapingSlash,bladesReach,shadowStep,umbralTrespass);;
     	
     	//Random entity
     	int initialHP = 1000;
@@ -35,22 +38,22 @@ public class Main {
     		//1 = auto, 2 = q, 3 = w, 4 = e, 5 = r, 6 = reset hp, 7 = exit
     		switch(option) {
     		case 1:
-    			Kayn.basicAttack(Kayn,entity);
+    			kayn.basicAttack(entity);
     			break;
     		case 2:
-    			Kayn.castQ(Kayn,entity);
+    			kayn.castQ(entity);
     			break;
     		case 3:
-    			Kayn.castW(Kayn,entity);
+    			kayn.castW(entity);
     			break;
     		case 4:
-    			Kayn.castE(Kayn,entity);
+    			kayn.castE(entity);
     			break;
     		case 5:
-    			Kayn.castUlt(Kayn,entity);
+    			kayn.castUlt(entity);
     			break;
     		case 6:
-    			entity.entityStats.setHP(initialHP);
+    			entity.getEntityStats().setHP(initialHP);
     			break;
     		case 7:
     			System.exit(0);
