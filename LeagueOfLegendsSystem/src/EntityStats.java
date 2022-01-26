f733681a574532f669cc8fb5d79736ab02aa9eb5
hamgod420 <84872216+hamgod420@users.noreply.github.com>
2022-01-25 21:17:37 -0500

    Update EntityStats.java

--- LeagueOfLegendsSystem/src/EntityStats.java
+++ LeagueOfLegendsSystem/src/EntityStats.java
@@ -5,7 +5,9 @@
  * EntityStats.java
  */
 public class EntityStats {
-	// Variables
+	/**
+	   * Attributes
+	   */
 	private int health;
 	private int attackDamage;
 	private int abilityPower;
@@ -16,7 +18,9 @@ public class EntityStats {
 	private int critchance;
 	private int movementSpeed;
 
-	// Default Constructor
+	/**
+	   * Constructors
+	   */
 	public EntityStats() {
 		health = 0;
 		attackDamage = 0;
@@ -28,8 +32,6 @@ public class EntityStats {
 		critchance = 0;
 		movementSpeed = 0;
 	}
-
-	// Constructor
 	public EntityStats(int hp, int ad, int ap, int ar, int mr, double as, int cd, int cr, int ms) {
 		health = hp;
 		attackDamage = ad;
@@ -42,67 +44,54 @@ public class EntityStats {
 		movementSpeed = ms;
 	}
 
-	// Getters and Setters
+	/**
+	   * Getters and setters
+	   */
 	public int getHP() {
 		return health;
 	}
-
 	public void setHP(int n) {
 		health = n;
 	}
-
 	public int getAD() {
 		return attackDamage;
 	}
-
 	public void setAD(int n) {
 		attackDamage = n;
 	}
-
 	public int getAP() {
 		return abilityPower;
 	}
-
 	public void setAP(int n) {
 		abilityPower = n;
 	}
-
 	public int getAR() {
 		return armor;
 	}
-
 	public void setAR(int n) {
 		armor = n;
 	}
-
 	public int getMR() {
 		return magicResist;
 	}
-
 	public void setMR(int n) {
 		magicResist = n;
 	}
-
 	public double getAS() {
 		return attackSpeed;
 	}
-
 	public void setAS(double d) {
 		attackSpeed = d;
 	}
-
 	public int getCrit() {
 		return critchance;
 	}
-
 	public void setCrit(int n) {
 		critchance = n;
 	}
-
 	public int getMS() {
 		return movementSpeed;
 	}
-
 	public void setMS(int n) {
 		movementSpeed = n;
 	}
